4fc28c9a6a7b1f27255a8477941bc15009a5fee3
PanMaster13 <36907215+PanMaster13@users.noreply.github.com>
2019-11-06 17:29:49 +0800

    contact page and product(index) page done
    only html with minor css

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Contact Us</title>
+</head>
+<body>
+	<h1>Contact Us</h1>
+	<p>Student Name: Jason Ang Chia Wuen</p>
+	<p>Student ID: 100087252</p>
+	<p>Email Address: <a href="mailto: 100087252@students.swinburne.edu.my">100087252@students.swinburne.edu.my</a></p>
+	
+	<p>This website is created mainly for educational and non-commercial use only. It is a partial fulfillment for completion of unit COS30020 - Web Application Development offered in Swinburne University of Technology, Sarawak Campus for Semester 2, 2019. The web-master and author(s) do not represent the business entity. The content of the pages of this website might be outdated or inaccurate, thus, the author(s) and web-master does not take any responsibility for incorrect information disseminated or cited from this website.</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>PC-Way</title>
+	<link rel="stylesheet" href="styles/style.css" />
+</head>
+<body>
+	<header>
+		<h1>Welcome to PC-Way</h1>
+		<p>The place for a computer & IT needs.</p>
+	</header>
+	<article>
+		<h2>Here are some of our featured products that we are selling.</h2>
+		<div id="product-box">
+			<div class="product-item">
+				<img src="images/keyboard-1.png" alt="Image of Cougar Attack X3 Keyboard" />
+				<p>Name: Cougar Attack X3 Gaming Keyboard</p>
+			</div>
+			<div class="product-item">
+				<img src="images/keyboard-2.png" alt="Image of Logitech G410 Atlas Keyboard" />
+				<p>Name: Logitech G410 Atlas Gaming Keyboard</p>
+			</div>
+			<div class="product-item">
+				<img src="images/mouse-1.png" alt="Image of Logitech G502 HERO Mouse" />
+				<p>Name: Logitech G502 HERO Gaming Mouse</p>
+			</div>
+			<div class="product-item">
+				<img src="images/mouse-2.png" alt="Image of Razer Lancehead Mouse" />
+				<p>Name: Razer Lancehead Gaming Mouse</p>
+			</div>
+			<div class="product-item">
+				<img src="images/mouse-pad-1.png" alt="Image of Qck Large Mousepad" />
+				<p>Name: Qck Large Gaming Mousepad</p>
+			</div>
+			<div class="product-item">
+				<img src="images/mouse-pad-2.png" alt="Image of Homemade Mousepad" />
+				<p>Name: Custom Designed Homemade Mousepad</p>
+			</div>
+			<div class="product-item">
+				<img src="images/laptop-1.png" alt="Image of a Classy Laptop" />
+				<p>Name: The one & only Classly Laptop</p>
+			</div>
+			<div class="product-item">
+				<img src="images/laptop-2.png" alt="Image of MacBook Air+ Laptop" />
+				<p>Name: The signature MacBook Air+ Laptop</p>
+			</div>
+			<div class="product-item">
+				<img src="images/card-1.png" alt="Image of Graphics card 1050 ti" />
+				<p>Name: NVIDIA GeForce 1050 Ti Graphics Card</p>
+			</div>
+			<div class="product-item">
+				<img src="images/card-2.png" alt="Image of Graphics card 1070" />
+				<p>Name: NVIDIA Gtx 1070 Graphics Card</p>
+			</div>
+		</div>
+	</article>
+</body>
+</html>
